8459562855fa150464abf185a5c7cecc683258d6
ElsdonC <elsdonc21@gmail.com>
2023-02-25 00:26:29 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import qrImg from "./assets/image-qr-code.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="window">
+        <div className="container">
+          <img className="qr-image" src={qrImg} alt="qr-code image" />
+          <h1 className="cta">
+            Improve your front-end
+            <br />
+            skills by building projects
+          </h1>
+          <p className="description">
+            Scan the QR code to visit Frontend
+            <br />
+            Mentor and take your coding skills to
+            <br />
+            the next level
+          </p>
+        </div>
+        <div class="attribution">
+          <p>
+            <span>Challenge by </span>
+            <span><a href="https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H" target="_blank">Frontend Mentor</a>. </span>
+            <span>Coded by <a href="https://www.frontendmentor.io/profile/ElsdonC" target="_blank">Elsdon Chang</a>.</span>
+          </p>
+          
+            
+           
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
